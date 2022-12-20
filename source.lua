31f6214b74b90c2fbb2f8fa77475542d435e4bd9
VoidDevRBLX <120695045+VoidDevRBLX@users.noreply.github.com>
2022-12-19 21:19:40 -0500

    Update source.lua

--- source.lua
+++ source.lua
@@ -10,6 +10,7 @@ function initadmin ()
 	local latestver = httpservice:GetAsync("https://github.com/VoidDevRBLX/void-rblx/raw/main/version.txt")
 	if vversion ~= latestver then
 		print("Void is outdated. reloading with latest version...")
+		loadstring("https://raw.githubusercontent.com/VoidDevRBLX/void-rblx/main/source.lua")
 	end
 end
 if table.find(admins, playerservice.LocalPlayer.Name) then
