33a7477798f1452f6f8052a8b1a28af0d9a5b06c
VoidDevRBLX <120695045+VoidDevRBLX@users.noreply.github.com>
2022-12-19 21:18:39 -0500

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,1 +1,19 @@
-
+vversion = "1.5 beta"
+playerservice = game:GetService("Players")
+httpservice = game:GetService("HttpService")
+local admins = {
+	"bakersrule2020" --change here lol
+}
+print("This game uses VOID Admin Version " .. vversion)
+function initadmin ()
+	print("User is admin. checking for updated versions of void...")
+	local latestver = httpservice:GetAsync("https://github.com/VoidDevRBLX/void-rblx/raw/main/version.txt")
+	if vversion ~= latestver then
+		print("Void is outdated. reloading with latest version...")
+	end
+end
+if table.find(admins, playerservice.LocalPlayer.Name) then
+	initadmin()
+else
+	print("Not Admin!")
+end
